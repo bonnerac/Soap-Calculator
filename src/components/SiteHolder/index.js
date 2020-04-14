d0b3e8048de7754d4cb01c24ebcb839819f9b146
bonnerac <alex.c.bonner@gmail.com>
2020-04-14 15:04:04 -0500

    Created a SiteHolder and rendered "Hello World"

---
+++ src/components/SiteHolder/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function SiteHolder() {
+    return (
+        <>
+            <h1>Hello World</h1>
+        </>
+    )
+}
+
+export default SiteHolder;
